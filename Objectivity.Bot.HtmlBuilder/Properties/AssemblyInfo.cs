ae5357be327192fbeb249679008e7f91b2a2ca02
Jacek Gratka <jgratka@objectivity.co.uk>
2017-10-02 15:03:11 +0200

    Fix nuspec file & AssemblyInfo.cs

--- Objectivity.Bot.HtmlBuilder/Properties/AssemblyInfo.cs
+++ Objectivity.Bot.HtmlBuilder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Objectiviy.Bot.HtmlBuilder2")]
+[assembly: AssemblyTitle("Objectiviy.Bot.HtmlBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Objectiviy.Bot.HtmlBuilder2")]
+[assembly: AssemblyCompany("Objectivity Bespoke Software Specialists")]
+[assembly: AssemblyProduct("Objectiviy.Bot.HtmlBuild")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
