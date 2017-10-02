f7254f88ec1baa46581067a8f997c9ddc46cd401
Jacek Gratka <jgratka@objectivity.co.uk>
2017-10-02 15:20:09 +0200

    update nuspec file

--- Update-Nuspec.ps1
+++ Update-Nuspec.ps1
@@ -1,6 +1,6 @@
 ﻿param([string]$version=1.0.0)
 $sfbDir = (get-item $PSScriptRoot )
-$appConfigFile = [IO.Path]::Combine($sfbDir, 'Objectivity.Bot.HtmlBuilder\Objectivity.Bot.HtmlBuilder.nuspec')
+$appConfigFile = [IO.Path]::Combine($sfbDir, 'Objectivity.Bot.HtmlBuilder\Objectivity.Bot.HtmlBuild.nuspec')
 $appConfig = New-Object XML;
 $appConfig.Load($appConfigFile);
 $appConfig.package.metadata.version = "$version";
